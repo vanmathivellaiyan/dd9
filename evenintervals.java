fd99fc8d79a9350125314ac1be387bab9e89560f
vanmathivellaiyan <vanmathivellaiyan1995@gmail.com>
2016-07-23 15:00:06 +0530

    Add files via upload

---
+++ evenintervals.java
@@ -0,0 +1,20 @@
+package aa;
+
+public class evenintervals {
+	public static void main(String args){
+		{
+		int i,j,k;
+		for(i=10;i<100;i++) 
+		{
+		         k=0;
+		         for(j=2;j<i;j++)
+		         {
+		                  if(i%j==0)
+		                  k++;
+		         }
+		         System.out.print(i+"   ");
+		}
+		}
+		}
+}
+
